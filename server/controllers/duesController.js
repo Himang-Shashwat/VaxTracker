8afaa4b5c864fe4dd70c37d05d6632d278bd7daf
Pranav Pore <pranav@pranavpore.com>
2023-12-04 12:53:16 +0530

    code cleanup

--- server/controllers/duesController.js
+++
@@ -1,8 +0,0 @@
-const Due = require('./../models/dueModel');
-const handlerFactory = require('./handlerFactory');
-
-exports.createDue = handlerFactory.createOne(Due);
-exports.getAllDues = handlerFactory.getAll(Due);
-exports.getDue = handlerFactory.getOne(Due);
-exports.updateDue = handlerFactory.update(Due);
-exports.deleteDue = handlerFactory.delete(Due);
--- server/controllers/expensesController.js
+++
@@ -1,8 +0,0 @@
-const Expense = require('./../models/expenseModel');
-const handlerFactory = require('./handlerFactory');
-
-exports.createExpense = handlerFactory.createOne(Expense);
-exports.getAllExpenses = handlerFactory.getAll(Expense);
-exports.getExpense = handlerFactory.getOne(Expense);
-exports.updateExpense = handlerFactory.update(Expense);
-exports.deleteExpense = handlerFactory.delete(Expense);
